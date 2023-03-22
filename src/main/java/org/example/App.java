234c21e62942614d830a104f92c604d6e51021cb
alex <kardulaalex@gmail.com>
2023-03-22 15:11:28 +0100

    Modified POM to contain necessary MySQL Driver, as well as started a local MySQL Database/Schema on my machine.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "SQLDriver!" );
+        try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test?serverTimezone=UTC", "root", "toor")) {
+            boolean isValid = con.isValid(0);
+
+            // "0" means disabling the timeout, when doing isValid checks
+            System.out.println("Do we have a valid db connection? = " + isValid);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
