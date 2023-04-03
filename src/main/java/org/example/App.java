0c1aaeff09fda6de04cbdac56ebf49318ad60e47
alex <kardulaalex@gmail.com>
2023-04-03 13:44:12 +0200

    Added logic pertaining to the the upate of the existing database. Then, added code for HikariDS dependency, allowing for a pool of connections. The first time we ask our HikariDataSource for a database connection, it will initialize a pool behind the scenes - which means opening up (by default) 10 database connections and giving you one of these ten.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import com.mysql.cj.protocol.Resultset;
+import com.zaxxer.hikari.HikariDataSource;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 /**
@@ -13,12 +15,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "SQLDriver!" );
-        try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test?serverTimezone=UTC", "root", "toor")) {
-            boolean isValid = con.isValid(0);
-
+        try(Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/test?serverTimezone=UTC", "root", "toor")) {
             // "0" means disabling the timeout, when doing isValid checks
+            boolean isValid = con.isValid(0);
             System.out.println("Do we have a valid db connection? = " + isValid);
-            Statement s = con.createStatement();
+
+            // Statement object used to generate and perform statements with the statement.execute("INSERT ...") fucntion
+            Statement statementGenerator = con.createStatement();
 
             // Any info about
             DatabaseMetaData dbmd = con.getMetaData();
@@ -26,16 +30,17 @@ public class App
 
             // If the student table exists!
             if (studentTables.next()){
-
+                // Do noting.
             }
             else {
-                s.execute("create table student ( stud_id integer, stud_name varchar(20) )");
-                s.execute("insert into student values(001,'Alex')");
-                s.execute("insert into student values(002,'Robert')");
-                s.execute("insert into student values(003,'Petar')");
+                statementGenerator.execute("create table student ( stud_id integer, stud_name varchar(20) )");
+                statementGenerator.execute("insert into student values(001,'Alex')");
+                statementGenerator.execute("insert into student values(002,'Robert')");
+                statementGenerator.execute("insert into student values(003,'Petar')");
+                statementGenerator.execute("insert into student values(004,'Robert')");
             }
 
-            ResultSet resultSet = s.executeQuery("select * from student");
+            ResultSet resultSet = statementGenerator.executeQuery("select * from student");
             if (resultSet != null) { // if rs == null, then there is no record in ResultSet to show
                 System.out.println("________________________________________");
                 while (resultSet.next()) // By this line we will step through our data row-by-row
@@ -46,13 +51,49 @@ public class App
                 }
             }
 
-            s.close(); // close the Statement to let the database know we're done with it
+            statementGenerator.close(); // close the Statement to let the database know we're done with it
 //            con.close(); // close the Connection to let the database know we're done with it, but including the driverManager within the try() does this automatically
 
+
+//-------------------------------------- More Proper Notation: ---------------------------------------------------------
+
+            // Shorthand for "introduce local variable" is Ctrl + Alt + V
+            PreparedStatement selectStatement = con.prepareStatement("SELECT * FROM STUDENT WHERE stud_name = ?");
+            selectStatement.setString(1,"Robert");
+            // this will return a ResultSet of all users with said name
+            ResultSet selectResult = selectStatement.executeQuery();
+
+            while (selectResult.next()){
+                System.out.println("Found Student: " + selectResult.getString("stud_name")
+                        + "with id: " + selectResult.getString("stud_id"));
+            }
+
+            PreparedStatement updateStatement = con.prepareStatement("UPDATE STUDENT " +
+                    "SET stud_name = 'NotPetar' WHERE stud_id = ?");
+            updateStatement.setString(1, "Petar");
+            int updatedRows = updateStatement.executeUpdate();  //executeUpdate() returns the number of updated rows, and is used for all statements that "change" the database
+            System.out.println("Rows updated: " + updatedRows);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        DataSource dataSource = createDataSource();
+        try (Connection hCon = dataSource.getConnection()) {
+            boolean isValid = hCon.isValid(0);
+            System.out.println("Do we have a valid HikariDB pool connection? = " + isValid);
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setJdbcUrl("jdbc:mysql://localhost/test?serverTimezone=UTC");
+        hikariDataSource.setUsername("root");
+        hikariDataSource.setPassword("toor");
+        return hikariDataSource;
     }
 }
